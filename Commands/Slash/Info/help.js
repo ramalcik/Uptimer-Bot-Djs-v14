a7b4e67071072f215dfbcf5f3ae704ab96a20df6
joltz1234 <75178291+joltz1234@users.noreply.github.com>
2024-02-28 16:33:42 -0600

    Update help.js

--- Commands/Slash/Info/help.js
+++ Commands/Slash/Info/help.js
@@ -25,16 +25,16 @@ module.exports = {
             .setTitle("🤖 Code Error Support & Bot Management")
             .setDescription("Need help? Check out the topics below!")
             .addFields(
-              { name: "<:dot:1206071454584668233> intent", value: "If you found an error related to Intents." },
-              { name: "<:dot:1206071454584668233> jsweb", value: "If you want to keep your JavaScript bot online for 24/7." },
-              { name: "<:dot:1206071454584668233> pyweb", value: "If you want to keep your Python bot online for 24/7." },
-              { name: "<:dot:1206071454584668233> ping", value: "Show You Bot's ping to respawning to commands" },
-              { name: "<:dot:1206071454584668233> invite", value: "invite bot to your own server" },
-              { name: "<:dot:1206071454584668233> moniter add", value: "add a moniter to the project to keep it 247 online" },
-              { name: "<:dot:1206071454584668233> moniter remove", value: "removes a moniter to the project to unkeep it 247 online" },
-              { name: "<:dot:1206071454584668233> moniter total", value: "shows all projects with a moniter" },
-             { name: "<:dot:1206071454584668233> moniter projects", value: "shows all of your projects with a moniter" },
-{ name: "<:dot:1206071454584668233> eval", value: "Evalulate a peice of code (Owner Only)" }
+              { name: "🔵 intent", value: "If you found an error related to Intents." },
+              { name: "🔵 jsweb", value: "If you want to keep your JavaScript bot online for 24/7." },
+              { name: "🔵 pyweb", value: "If you want to keep your Python bot online for 24/7." },
+              { name: "🔵 ping", value: "Show You Bot's ping to respawning to commands" },
+              { name: "🔵 invite", value: "invite bot to your own server" },
+              { name: "🔵 moniter add", value: "add a moniter to the project to keep it 247 online" },
+              { name: "🔵 moniter remove", value: "removes a moniter to the project to unkeep it 247 online" },
+              { name: "🔵 moniter total", value: "shows all projects with a moniter" },
+              { name: "🔵 moniter projects", value: "shows all of your projects with a moniter" },
+              { name: "🔵 eval", value: "Evalulate a peice of code (Owner Only)" }
             )
         ]
       });
