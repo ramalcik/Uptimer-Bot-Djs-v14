0cdfae8b7bf5fdca12780d7a808d85f8bc524293
joltz1234 <75178291+joltz1234@users.noreply.github.com>
2024-02-28 16:27:05 -0600

    Update embed.js

--- Assets/Config/embed.js
+++ Assets/Config/embed.js
@@ -4,7 +4,7 @@ module.exports = {
   stanbycolor: "#00FF00",
   wrongcolor: "#00FF00",
   footertext: "Powered by 𝓙𝓸𝓵𝓽𝔃",
-  footericon: "https://cdn.discordapp.com/attachments/1206070092824059924/1206070141524254750/G78hRYJ.png?ex=65daab59&is=65c83659&hm=e73e4e5cfbcdb4decb3a69aef696e1dc052d3ead7134e2bc7d1c26f85edcd7b8&",
-  authoricon: "https://cdn.discordapp.com/attachments/1206070092824059924/1206070141524254750/G78hRYJ.png?ex=65daab59&is=65c83659&hm=e73e4e5cfbcdb4decb3a69aef696e1dc052d3ead7134e2bc7d1c26f85edcd7b8&",
-  loadingicon: "https://cdn.discordapp.com/attachments/1206070092824059924/1206070141524254750/G78hRYJ.png?ex=65daab59&is=65c83659&hm=e73e4e5cfbcdb4decb3a69aef696e1dc052d3ead7134e2bc7d1c26f85edcd7b8&"
+  footericon: "",
+  authoricon: "",
+  loadingicon: ""
 }
