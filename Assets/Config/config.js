9a61364564e8554ae4fbafff184736758ebe1451
joltz1234 <75178291+joltz1234@users.noreply.github.com>
2024-02-28 16:23:39 -0600

    Update config.js

--- Assets/Config/config.js
+++ Assets/Config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  TOKEN: process.env.TOKEN || "MTIwNjA2OTg5OTQyNDgyNTQ0NA.GRBcJh.E2armkhwa9OpiIS3n3-2rnhdUnbnLyWlK4JJDQ",
-  MONGO_DB: process.env.MONGO_DB || "mongodb+srv://joltzx123:c4n4fs76H496aOk6@cluster0.hswwy2r.mongodb.net/?retryWrites=true&w=majority",
-  CLIENT_ID: process.env.CLIENT_ID || "1206069899424825444",
-  OWNERS: ["837162825373188106"],
-  SUPPORT_SERVER: "1203152712343093288",
+  TOKEN: process.env.TOKEN || "",
+  MONGO_DB: process.env.MONGO_DB || "",
+  CLIENT_ID: process.env.CLIENT_ID || "",
+  OWNERS: [""],
+  SUPPORT_SERVER: "",
   CHANNELS: {
-    COMMANDS_LOGS: "1203441337878450206",
-    ERROR_COMMAND_LOGS: "1203441337878450206"
+    COMMANDS_LOGS: "",
+    ERROR_COMMAND_LOGS: ""
   }
 }
